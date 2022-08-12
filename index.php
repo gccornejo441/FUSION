2f8a222201108266801668e33803a8da09eeadfe
Gabriel Cornejo <64950833+gccornejo441@users.noreply.github.com>
2022-08-12 15:37:09 -0700

    CREATING INDEX.PHP

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="actions.php?table_name=clients&action_type=add" method="post">
+Name: <input type="text" name="name"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
